2cd39af58f22aec3f5adfc9c24f46898af83983f
Claire Harmer <claire.f.harmer@gmail.com>
2018-07-26 18:22:54 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -4,11 +4,12 @@ class Shoe
   attr_accessor  :color, :size, :material, :condition
   attr_reader :brand
 
- def initialize(brand)
+  def initialize(brand)
     @brand = brand
-end
-def cobble
-puts "Your shoe is as good as new!"
-@codition = "new"
-end
+  end
+
+  def cobble
+  puts "Your shoe is as good as new!"
+  @codition = "new"
+  end
 end
